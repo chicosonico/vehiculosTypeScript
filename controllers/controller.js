c6721ba861e2d913a79649d5ba72d31bb02c111d
chicosonico <schvarzman@gmail.com>
2020-01-15 12:21:05 +0100

    order changed  on function (brand to color)

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,6 @@
 "use strict";
 function createCar(plate, brand, color) {
-  
+    var car = new Car(plate, brand, color);
     car.addWheel(new Wheel(2, "SEAT"));
     document.body.innerText = "CAR: PLATE: " + car.plate
         + " COLOR: " + car.color + " BRAND: " + brand
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,6 +1,6 @@
 
 function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
+    let car=new Car(plate,brand,color);
     car.addWheel(new Wheel(2,"SEAT"));
     document.body.innerText="CAR: PLATE: " + car.plate 
     + " COLOR: " +car.color + " BRAND: " + brand 
