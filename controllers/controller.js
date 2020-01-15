00d4e92619e65c0dd71e1524fc3cbf2c9bbf0704
marinucs <ferrergarridom@gmail.com>
2020-01-15 09:46:33 +0100

    First commit
    All files given from IT Academy and de VS workplace

---
+++ controllers/controller.js
@@ -0,0 +1,8 @@
+"use strict";
+function createCar(plate, brand, color) {
+  
+    car.addWheel(new Wheel(2, "SEAT"));
+    document.body.innerText = "CAR: PLATE: " + car.plate
+        + " COLOR: " + car.color + " BRAND: " + brand
+        + " WHEELS: " + car.wheels;
+}
