4ffd23eef26cd5fa2cb40e328eea6d048134de0e
chicosonico <schvarzman@gmail.com>
2020-01-15 12:30:45 +0100

    Data input working well

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,9 @@
 
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,brand,color);
+function createCar(plate:string,color:string,brand:string){
+    let car=new Car(plate,color,brand);
     car.addWheel(new Wheel(2,"SEAT"));
     document.body.innerText="CAR: PLATE: " + car.plate 
-    + " COLOR: " +car.color + " BRAND: " + brand 
+    + " COLOR: " + car.color + " BRAND: " + car.brand 
     + " WHEELS: " + car.wheels;
 }
 
