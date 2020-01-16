292d1f612409bc52e8efd6cc1da3ef8c64b66051
chicosonico <schvarzman@gmail.com>
2020-01-16 12:32:10 +0100

    car created form fixed

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,6 +4,14 @@ function createCar(plate, color, brand) {
     car.addWheel(new Wheel(2, "SEAT"));
     var formInput = document.getElementById("formInput");
     formInput.style.display = "none";
+    var carCreated = document.getElementById("carCreated");
+    carCreated.style.display = "block";
+    var plateRes = document.getElementById("plateRes");
+    plateRes.innerHTML = "Plate: " + car.plate;
+    var brandRes = document.getElementById("brandRes");
+    brandRes.innerHTML = "Brand: " + car.brand;
+    var colorRes = document.getElementById("colorRes");
+    colorRes.innerHTML = "Color: " + car.color;
     // document.body.innerText="CAR: PLATE: " + car.plate 
     // + " COLOR: " + car.color + " BRAND: " + car.brand;
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -2,8 +2,22 @@
 function createCar(plate:string,color:string,brand:string){
     let car=new Car(plate,color,brand);
     car.addWheel(new Wheel(2,"SEAT"));
+
     let formInput = document.getElementById("formInput") as HTMLElement;
    formInput.style.display = "none";
+
+   let carCreated = document.getElementById("carCreated") as HTMLElement;
+   carCreated.style.display = "block";
+
+   let plateRes = document.getElementById("plateRes") as HTMLElement;
+   plateRes.innerHTML = "Plate: " + car.plate ;
+
+   let brandRes = document.getElementById("brandRes") as HTMLElement;
+   brandRes.innerHTML = "Brand: " + car.brand ;
+
+   let colorRes = document.getElementById("colorRes") as HTMLElement;
+   colorRes.innerHTML = "Color: " + car.color ;
+   
     // document.body.innerText="CAR: PLATE: " + car.plate 
     // + " COLOR: " + car.color + " BRAND: " + car.brand;
 }
