73c3b015535bd600c9935c7c6f68fe287eedb14b
chicosonico <schvarzman@gmail.com>
2020-01-20 13:32:17 +0100

    AddWheels form gets the input but does not show it

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,11 @@
 "use strict";
 function createCar(plate, color, brand) {
     var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
+    // car.addWheel(new Wheel(2,"SEAT"));
     var formInput = document.getElementById("formInput");
     formInput.style.display = "none";
-    var carCreated = document.getElementById("carCreated");
-    carCreated.style.display = "block";
+    var addWheels = document.getElementById("addWheels");
+    addWheels.style.display = "block";
     var plateRes = document.getElementById("plateRes");
     plateRes.innerHTML = "Plate: " + "<b>" + car.plate + "</b>";
     var brandRes = document.getElementById("brandRes");
@@ -15,3 +15,49 @@ function createCar(plate, color, brand) {
     // document.body.innerText="CAR: PLATE: " + car.plate 
     // + " COLOR: " + car.color + " BRAND: " + car.brand;
 }
+function addWheels(diamWheel1, diamWheel2, diamWheel3, diamWheel4, brandWheel1, brandWheel2, brandWheel3, brandWheel4) {
+    var wheel1 = new Wheel(diamWheel1, brandWheel1);
+    var wheel2 = new Wheel(diamWheel2, brandWheel2);
+    var wheel3 = new Wheel(diamWheel3, brandWheel3);
+    var wheel4 = new Wheel(diamWheel4, brandWheel4);
+    var formInput = document.getElementById("formInput");
+    formInput.style.display = "none";
+    var addWheels = document.getElementById("addWheels");
+    addWheels.style.display = "none";
+    var showCarCreated = document.getElementById("showCarCreated");
+    showCarCreated.style.display = "block";
+    var diamW4 = document.getElementById("diamWheel4");
+    diamW4.innerHTML = "Diameter W 4: " + "<b>" + wheel4.diameter + "</b>";
+    console.log(wheel4);
+    var plateRes2 = document.getElementById("plateRes2");
+    plateRes2.innerHTML = "Plate: " + "<b>" + car.plateRes2 + "</b>";
+    console.log(plateRes2);
+    //    let wheel1diam = document.getElementById("diamWheel1") as HTMLElement;
+    //    diamWheel1.innerHTML = "Diameter W 1: " + "<b>" + wheel1.diamWheel1 + "</b>";
+    //    let brandRes = document.getElementById("brandRes") as HTMLElement;
+    //    brandRes.innerHTML = "Brand: " + "<b>" + "Test" + "</b>";
+    //    let colorRes = document.getElementById("colorRes") as HTMLElement;
+    //    colorRes.innerHTML = "Color: " + "<b>" + car.color + "</b>";
+}
+// function showCarCreated(){
+//    let formInput = document.getElementById("formInput") as HTMLElement;
+//    formInput.style.display = "none";
+//    let addWheels = document.getElementById("addWheels") as HTMLElement;
+//    addWheels.style.display = "none";
+//    let showCarCreated = document.getElementById("showCarCreated") as HTMLElement;
+//    showCarCreated.style.display = "block";
+// }
+// let plateRes = document.getElementById("plateRes") as HTMLElement;
+// plateRes.innerHTML = "Plate: " + "<b>" + car.plate + "</b>";
+// let brandRes = document.getElementById("brandRes") as HTMLElement;
+// brandRes.innerHTML = "Brand: " + "<b>" + car.brand + "</b>";
+// let colorRes = document.getElementById("colorRes") as HTMLElement;
+// colorRes.innerHTML = "Color: " + "<b>" + car.color + "</b>";
+// Client code
+// function createCar(plate:string,brand:string,color:string){
+//     let car=new Car(plate,color,brand);
+//     car.addWheel(new Wheel(2,"SEAT"));
+//     document.body.innerText="CAR: PLATE: " + car.plate 
+//     + " COLOR: " +car.color + " BRAND: " + brand 
+//     + " WHEELS: " + JSON.stringify(car.wheels);
+// }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,7 +1,7 @@
 
 function createCar(plate:string,color:string,brand:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
+    var car =new Car(plate,color,brand);
+    // car.addWheel(new Wheel(2,"SEAT"));
 
     let formInput = document.getElementById("formInput") as HTMLElement;
    formInput.style.display = "none";
@@ -9,7 +9,7 @@ function createCar(plate:string,color:string,brand:string){
    let addWheels = document.getElementById("addWheels") as HTMLElement;
    addWheels.style.display = "block";
 
-   let plateRes = document.getElementById("plateRes") as HTMLElement;
+  var plateRes = document.getElementById("plateRes") as HTMLElement;
    plateRes.innerHTML = "Plate: " + "<b>" + car.plate + "</b>";
 
    let brandRes = document.getElementById("brandRes") as HTMLElement;
@@ -21,4 +21,79 @@ function createCar(plate:string,color:string,brand:string){
     // document.body.innerText="CAR: PLATE: " + car.plate 
     // + " COLOR: " + car.color + " BRAND: " + car.brand;
 }
+
+
+function addWheels(
+    diamWheel1:number, diamWheel2:number, diamWheel3:number, diamWheel4:number,
+    brandWheel1:string, brandWheel2:string, brandWheel3:string, brandWheel4:string,){
+    
+    let wheel1 = new Wheel(diamWheel1, brandWheel1);
+    let wheel2 = new Wheel(diamWheel2, brandWheel2);
+    let wheel3 = new Wheel(diamWheel3, brandWheel3);
+    let wheel4 = new Wheel(diamWheel4, brandWheel4);
+
+    let formInput = document.getElementById("formInput") as HTMLElement;
+   formInput.style.display = "none";
+
+   let addWheels = document.getElementById("addWheels") as HTMLElement;
+   addWheels.style.display = "none";
+
+   let showCarCreated = document.getElementById("showCarCreated") as HTMLElement;
+   showCarCreated.style.display = "block";
+
+   let diamW4 = document.getElementById("diamWheel4") as HTMLElement;
+   diamW4.innerHTML = "Diameter W 4: " + "<b>" + wheel4.diameter + "</b>";
+   console.log(wheel4);
+   
+
+   let plateRes2 = document.getElementById("plateRes2") as HTMLElement;
+   plateRes2.innerHTML = "Plate: " + "<b>" + car.plateRes2 + "</b>";
+   console.log(plateRes2);
+
+//    let wheel1diam = document.getElementById("diamWheel1") as HTMLElement;
+//    diamWheel1.innerHTML = "Diameter W 1: " + "<b>" + wheel1.diamWheel1 + "</b>";
+
+//    let brandRes = document.getElementById("brandRes") as HTMLElement;
+//    brandRes.innerHTML = "Brand: " + "<b>" + "Test" + "</b>";
+
+//    let colorRes = document.getElementById("colorRes") as HTMLElement;
+//    colorRes.innerHTML = "Color: " + "<b>" + car.color + "</b>";
+}
+
+
+
+
+
+
+// function showCarCreated(){
+     
+//    let formInput = document.getElementById("formInput") as HTMLElement;
+//    formInput.style.display = "none";
+
+//    let addWheels = document.getElementById("addWheels") as HTMLElement;
+//    addWheels.style.display = "none";
+
+//    let showCarCreated = document.getElementById("showCarCreated") as HTMLElement;
+//    showCarCreated.style.display = "block";
+     
+// }
+
+// let plateRes = document.getElementById("plateRes") as HTMLElement;
+// plateRes.innerHTML = "Plate: " + "<b>" + car.plate + "</b>";
+
+// let brandRes = document.getElementById("brandRes") as HTMLElement;
+// brandRes.innerHTML = "Brand: " + "<b>" + car.brand + "</b>";
+
+// let colorRes = document.getElementById("colorRes") as HTMLElement;
+// colorRes.innerHTML = "Color: " + "<b>" + car.color + "</b>";
+
+
+// Client code
+// function createCar(plate:string,brand:string,color:string){
+//     let car=new Car(plate,color,brand);
+//     car.addWheel(new Wheel(2,"SEAT"));
+//     document.body.innerText="CAR: PLATE: " + car.plate 
+//     + " COLOR: " +car.color + " BRAND: " + brand 
+//     + " WHEELS: " + JSON.stringify(car.wheels);
+// }
 
